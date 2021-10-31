6ac3ae02ace79faf0970af6a4c95bb1041b4feb1
th368 <tom_hepworth@hotmail.co.uk>
2021-10-31 15:05:48 +0000

    add house info

--- pq_scraper/pq_scraper/spiders/PQSpider.py
+++ pq_scraper/pq_scraper/spiders/PQSpider.py
@@ -7,7 +7,8 @@ class PQSpider(CrawlSpider):
     name = "PQSpider"
 
     allowed_domains = ["questions-statements.parliament.uk"]
-    start_urls = ["https://questions-statements.parliament.uk/written-questions?SearchTerm=&DateFrom=01%2F01%2F1990&DateTo=29%2F10%2F2021&AnsweredFrom=&AnsweredTo=&House=Commons&Answered=Answered&Expanded=True"] # this is the full set of PQs on record...
+    start_urls = ["https://questions-statements.parliament.uk/written-questions?SearchTerm=&DateFrom=01%2F01%2F1990&DateTo=29%2F10%2F2021&AnsweredFrom=&AnsweredTo=&House=Bicameral&Answered=Answered&Expanded=True"] # this is the full set of PQs on record...
+
 
     rules = (
         Rule(LinkExtractor(allow='written-questions', deny='detail')),
@@ -22,8 +23,6 @@ class PQSpider(CrawlSpider):
 
         # pull out department info
         department = re.sub('(?i)question for ', '' , response.xpath('//*[@id="main-content"]/div[1]/div/div[2]/div/div/h2/text()').get().strip())
-
-        # extract hansard header
 
         # pull out our answer text
         # the question seems to be housed within "col-lg-12 read-more"
@@ -43,7 +42,8 @@ class PQSpider(CrawlSpider):
             mp_names = [response.xpath('//*[@class="primary-info"]/text()')[num].get().strip() for num in range(2)]
             political_parties = [response.xpath('//*[@class="tertiary-info"]/text()')[num].get().strip() for num in range(2)]
             constituency = [response.xpath('//*[@class="indicator indicator-label"]/text()')[num].get().strip() for num in range(2)]
-
+            house = response.xpath('//*[@class="info-inner"]/div[2]/div[1]/text()').getall()
+            
             yield {
                 'department': department,
                 'uin': uin,
@@ -57,5 +57,6 @@ class PQSpider(CrawlSpider):
                 'respondent': mp_names[1],
                 'respondent_party': political_parties[1],
                 'respondent_constituency': constituency[1],
-                'answer_date': answer_date
+                'answer_date': answer_date,
+                'house': "".join([h.strip() for h in set(house)]) 
             }
